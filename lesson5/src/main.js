99483aad179a58f9695d6b3ae98c3df7878bad64
yoshiwaki <wakk12107@au.com>
2021-12-05 17:54:28 +0900

    hello worldの表示

--- lesson5/src/main.js
+++ lesson5/src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import router from './router'
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,//routingのために追加
+  //routingのために追加
   render: h => h(App),//index.htmlにApp.vueを描写している
 }).$mount('#app')
